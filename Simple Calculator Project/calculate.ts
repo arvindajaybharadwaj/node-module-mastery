ef86041d5ed4c750082492ca517d1b6a381f830c
arvindajaybharadwaj <arvindajayarts@gmail.com>
2024-10-20 12:58:05 +0530

    Defining the math functions

--- Simple Calculator Project/calculate.ts
+++ Simple Calculator Project/calculate.ts
@@ -1,1 +1,24 @@
 // Contains functions to perform calculations
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+}
