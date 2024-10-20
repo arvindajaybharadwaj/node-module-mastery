07e6a6637d0c3493df5e7e61224793f5b26d28e7
arvindajaybharadwaj <arvindajayarts@gmail.com>
2024-10-20 14:23:46 +0530

    Updating calculate.ts

--- Simple Calculator Project/calculate.ts
+++ Simple Calculator Project/calculate.ts
@@ -1,24 +1,17 @@
 // Contains functions to perform calculations
 
-function add(a: number, b: number): number {
+export function add(a: number, b: number): number {
     return a + b;
 }
 
-function subtract(a: number, b: number): number {
+export function subtract(a: number, b: number): number {
     return a - b;
 }
 
-function multiply(a: number, b: number): number {
+export function multiply(a: number, b: number): number {
     return a * b;
 }
 
-function divide(a: number, b: number): number {
+export function divide(a: number, b: number): number {
     return a / b;
-}
-
-module.exports = {
-    add,
-    subtract,
-    multiply,
-    divide,
 }
