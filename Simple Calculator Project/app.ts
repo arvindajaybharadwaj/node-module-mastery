2f92ab3f5b51531eb4bb43fa6b819b9d78840f04
arvindajaybharadwaj <arvindajayarts@gmail.com>
2024-10-20 14:24:50 +0530

    Finishing Up

--- Simple Calculator Project/app.ts
+++ Simple Calculator Project/app.ts
@@ -1,1 +1,37 @@
 // Main App
+
+import * as input from "./input";
+import * as calculate from "./calculate";
+
+async function app() {
+    const operation = await input.getUserInput("Enter the operation you want to perform: (add, subtract, multiply, divide)\n");
+    const num1 = parseFloat(await input.getUserInput("Enter the first number: \n"));
+    const num2 = parseFloat(await input.getUserInput("Enter the second number: \n"));
+
+    let result;
+
+    switch(operation) {
+        case "add": {
+            result = calculate.add(num1, num2);
+            break;
+        }
+        case "subtract": {
+            result = calculate.subtract(num1, num2);
+            break;
+        }
+        case "multiply": {
+            result = calculate.multiply(num1, num2);
+            break;
+        }
+        case "divide": {
+            result = calculate.divide(num1, num2);
+            break;
+        }
+        default: {
+            console.log("Error! Please enter a valid operation");
+        }
+    }
+    console.log(`The result is: ${result}`);
+}
+
+app();
